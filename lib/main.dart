5f11c974b33788029203b7e7ab3252204bb8a511
Hong Jian Wong <hjwongatwork@gmail.com>
2024-12-09 20:32:48 +0800

    Test fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       title: 'Gamuda Flutter App',
       home: LoginPage(),
-    ),
-  );
+    );
+  }
 }
